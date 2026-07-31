d4fc55df1596ee50079a0fa87d1adc052ad2ddf9
adambaker08 <adambaker141094@live.co.uk>
2026-07-31 20:49:02 +0100

    Add sitemap.js for generating site map
    Implement sitemap generation with static and dynamic routes.

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+import { getAllPosts } from "@/lib/posts";
+
+const SITE_URL = "https://golfislife-sigma.vercel.app";
+
+export default function sitemap() {
+  const staticRoutes = [
+    "",
+    "/blog",
+    "/about",
+    "/privacy-policy",
+    "/affiliate-disclosure",
+  ].map((route) => ({
+    url: ${SITE_URL}${route},
+    lastModified: new Date(),
+  }));
+
+  const posts = getAllPosts();
+  const postRoutes = posts.map((post) => ({
+    url: ${SITE_URL}/blog/${post.slug},
+    lastModified: post.date ? new Date(post.date) : new Date(),
+  }));
+
+  return [...staticRoutes, ...postRoutes];
+}
